3aaced438bbd16479f7a26d8246892fa66ce437f
dy9amix <segunorodeji@gmail.com>
2018-02-10 23:29:42 +0100

    first commit

---
+++ pagetwo/myscript.js
@@ -0,0 +1,9 @@
+var jq = $.noConflict();
+jq(document).ready(function(){
+    jq("#closex").click(function(){
+    	jq(".zeedas-side3").hide();}
+    	);
+    jq("#show").click(function(){
+    	jq(".zeedas-side3").show();}
+    	);
+    });
